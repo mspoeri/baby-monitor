0cacfff12423b592f16f341f685a80a282463a9f
Markus Spöri <mspoeri@die-softwareklitsche.de>
2024-02-08 00:00:48 +0100

    perf(audio-server): uses shared ffmpeg stream

--- audio-server/cmd/ffmpeg-proxy/main.go
+++ audio-server/cmd/ffmpeg-proxy/main.go
@@ -3,71 +3,76 @@ package main
 import (
 	"io"
 	"log/slog"
-	"net"
 	"net/http"
 	"os"
 	"os/exec"
-	"os/signal"
-	"syscall"
+	"sync"
+)
+
+var (
+	ffmpegStdout io.ReadCloser
+	ffmpegCmd    *exec.Cmd
+	buffer       = make([]byte, 0)
+	bufferMutex  = &sync.RWMutex{}
 )
 
 func main() {
+	ffmpegCmd = exec.Command("ffmpeg", "-ar", "44100", "-ac", "1", "-f", "alsa", "-i", "plughw:1,0", "-f", "wav", "pipe:1")
+	var err error
+	ffmpegStdout, err = ffmpegCmd.StdoutPipe()
+	if err != nil {
+		slog.Error("StdoutPipe error:", err)
+		os.Exit(1)
+		return
+	}
+
+	err = ffmpegCmd.Start()
+	if err != nil {
+		slog.Error("ffmpeg error:", err)
+		os.Exit(1)
+		return
+	}
+
+	go func() {
+		for {
+			tmp := make([]byte, 1024)
+			n, err := ffmpegStdout.Read(tmp)
+			if err != nil {
+				slog.Error("Read error:", err)
+				os.Exit(1)
+			}
+
+			bufferMutex.Lock()
+			buffer = append(buffer, tmp[:n]...)
+			bufferMutex.Unlock()
+		}
+	}()
+
 	http.HandleFunc("/stream", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding")
 
-		cmd := exec.Command("ffmpeg", "-ar", "44100", "-ac", "1", "-f", "alsa", "-i", "plughw:1,0", "-f", "wav", "pipe:1")
-		stdout, err := cmd.StdoutPipe()
-		if err != nil {
-			slog.Error("StdoutPipe error:", err)
-			return
-		}
-
-		w.Header().Set("Content-Type", "audio/wav")
-
-		err = cmd.Start()
-		if err != nil {
-			slog.Error("ffmpeg error:", err)
-			err = cmd.Process.Signal(syscall.SIGTERM)
-			if err != nil {
-				slog.Error("Signal error:", err)
-			}
-			return
-		}
-
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 		go func() {
-			<-c
-			err := cmd.Process.Signal(syscall.SIGTERM)
-			if err != nil {
-				slog.Error("Signal error:", err)
+			localBuffer := make([]byte, 0)
+			for {
+				bufferMutex.RLock()
+				if len(buffer) > len(localBuffer) {
+					newBytes := buffer[len(localBuffer):]
+					localBuffer = append(localBuffer, newBytes...)
+				}
+				bufferMutex.RUnlock()
+
+				if len(localBuffer) > 0 {
+					_, err := w.Write(localBuffer)
+					if err != nil {
+						slog.Error("Write error:", err)
+						return
+					}
+					localBuffer = localBuffer[:0]
+				}
 			}
 		}()
-
-		go func() {
-			defer func(stdout io.ReadCloser) {
-				err := stdout.Close()
-				if err != nil {
-					slog.Error("Close error:", err)
-				}
-			}(stdout)
-			_, err := io.Copy(w, stdout)
-			if err != nil {
-				if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-					slog.Error("io.Copy timeout error:", err)
-				} else {
-					slog.Error("io.Copy error:", err)
-				}
-				return
-			}
-		}()
-
-		err = cmd.Wait()
-		if err != nil {
-			slog.Error("ffmpeg exited with error:", err)
-		}
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -76,7 +81,7 @@ func main() {
 	})
 
 	slog.Info("Server is listening on port 8081")
-	err := http.ListenAndServe(":8081", nil)
+	err = http.ListenAndServe(":8081", nil)
 	if err != nil {
 		slog.Error("ListenAndServe error:", err)
 		os.Exit(1)
